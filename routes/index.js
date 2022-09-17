4e9a35d6904e7a9d746b004fcd4406df8225f1ba
hol16046 <hol16046@byui.edu>
2022-09-17 00:05:13 -0600

    lesson 02

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const router = require('./contacts');
 const routes = express.Router();
 
 routes.use('/contacts', require('./contacts'))
 
-module.exports = routes;
+module.exports = router;
