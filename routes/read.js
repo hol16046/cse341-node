948c39e8ef75f9fbe78d1a6ee5043036000299b8
hol16046 <hol16046@byui.edu>
2022-10-01 23:27:58 -0600

    Lesson 06

--- routes/read.js
+++ routes/read.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const readController = require('../controllers/read');
+const validation = require('../middleware/validate');
 
 router.get('/', readController.getAll);
 
-router.post('/', readController.createBook);
+router.post('/', validation.saveBook, readController.createBook);
+
+router.put('/:id', validation.saveBook, readController.updateBook);
+
+router.delete('/:id', readController.deleteBook);
 
 module.exports = router;
