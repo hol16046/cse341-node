83f7d79017c5e1beb496b12d8ac1ee64a4075a7f
hol16046 <hol16046@byui.edu>
2022-10-08 22:28:13 -0600

    Lesson 08

--- app.js
+++ app.js
@@ -19,13 +19,13 @@ const config = {
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
-app.get('/profile', requiresAuth(), (req, res) => {
-    res.send(JSON.stringify(req.oidc.user));
-});
-
 // req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
 });
 
 app
