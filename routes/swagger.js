ae9fe3925ff830a8ebc6b3871a3ca914f2d087b6
hol16046 <hol16046@byui.edu>
2022-10-08 22:45:38 -0600

    Lesson 08

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
+const { requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', requiresAuth(), swaggerUi.setup(swaggerDocument));
 
